6b69e34f50c1e68a89435fcea391d783031728ef
vibicer <100922360+vibicer@users.noreply.github.com>
2024-03-23 04:11:25 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='mycalculator',
+    version='1.0.0',
+    packages=find_packages(),
+    description='A simple calculator package',
+    author='Vibicer',
+    author_email='vibicer@gmail.com',
+    url='https://github.com/vibicer/mycalculator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
